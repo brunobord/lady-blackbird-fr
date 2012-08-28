373b0bc24c93132a8056bf685ff32d8d66c503dc
Bruno Bord <bruno@jehaisleprintemps.net>
2012-08-28 22:41:11 +0200

    silly tools

---
+++ tools/names.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""Find names
+"""
+import random
+
+MALE = ["Abel", "Artemis", "August", "Eli", "Giovanni", "Ivan", "Jack", "Jefferson",
+"Jonas", "Leo", "Logan", "Malachi", "Mario", "Micah", "Nahum", "Noah", "Orlence", "Oscar", "Samuel",
+"Silas", "Victor", "Vlad", "Wester"]
+
+FEMALE = ["Alice", "Ardent", "Ashlyn", "Caess", "Clare", "Elena", "Eveline", "Fiona",
+"Grace", "Hannah", "Hazel", "Hester", "Isabel", "Krista", "Jezebel", "Leah", "Lucile", "Lydia",
+"Seraphina", "Sonya", "Sophie", "Veronica", "Violet"]
+
+SURNAME = ["Bell", "Bowen", "Canter", "Carson", "Cross", "Harwood", "Hollas",
+"Hunter", "Kalra", "Keel", "Moreau", "Morgan", "Porter", "Pickett", "Quinn", "Sidhu", "Soto",
+"Torrez", "Vakharia", "Walker", "Winter", "Wright"]
+
+NOBLE = ["Ash", "Blackbird", "Firefly", "Mooncloud", "Nightsong", "Snow",
+"Twilight", "Whitethorn"]
+
+if __name__ == '__main__':
+    male = raw_input('Male or Female? (m/F) ').lower() == 'm'
+    noble = raw_input('Noble? (y/N)').lower() == 'y'
+    if male:
+        first_name = random.choice(MALE)
+    else:
+        first_name = random.choice(FEMALE)
+    if noble:
+        surname = random.choice(NOBLE)
+    else:
+        surname = random.choice(SURNAME)
+
+    print("%s %s" % (first_name, surname))
