bd75183c80097f6e8d1d9715a83e82b7ac790608
Bruno Bord <bruno@jehaisleprintemps.net>
2012-09-05 22:48:43 +0200

    tool: nettoyage du build

--- fabfile.py
+++ fabfile.py
@@ -2,9 +2,18 @@ import os
 from fabric.api import local, lcd
 
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+BUILD_PATH = os.path.join(ROOT_DIR, '_build', 'html')
 
 
 def serve():
     "Serve local website for preview purposes"
-    with lcd(os.path.join(ROOT_DIR, '_build', 'html')):
+    with lcd(BUILD_PATH):
         local('python -m SimpleHTTPServer')
+
+
+def clean():
+    "Clean build dir without breaking everything"
+    with lcd(BUILD_PATH):
+        local('rm -Rf _images _sources aventure _static jeu')
+        local('rm -f *')
+        local('rm -f .buildinfo')
